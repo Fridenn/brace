3a958bc35c6cd8e2a866a08e697b7a75f577ba30
Vladimir Shumchenia <fridenn@tut.by>
2016-12-28 17:00:07 +0300

    Try fix php mode.

--- mode/php.js
+++ mode/php.js
@@ -12778,7 +12778,7 @@ oop.inherits(Mode, HtmlMode);
 (function() {
 
     this.createWorker = function(session) {
-        var worker = new WorkerClient(["ace"], "ace/mode/php_worker", "PhpWorker");
+        var worker = new WorkerClient(["ace"], require("ace/mode/php_worker"), "PhpWorker");
         worker.attachToDocument(session.getDocument());
 
         if (this.inlinePhp)
